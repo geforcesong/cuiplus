f9d0278a014a7d4ae1d2c0ea522296d052b80e6b
gguo <gguo@movoto.com>
2018-04-04 20:39:09 -0700

    added watch model

--- webpack.config.js
+++ webpack.config.js
@@ -44,6 +44,11 @@ module.exports = {
             }
         ]
     },
+    watchOptions: {
+        poll: 1000, //监测修改的时间(ms)
+        aggregateTimeout: 500, //防止重复按键，500毫米内算按键一次
+        ignored: /node_modules/ //不监测
+    },
     plugins: [
         new ExtractTextPlugin(`cui.${pkg.version}.css`),
         new Webpack.ProvidePlugin({
